ddf0bb4b1e7980ee980b5e16e581f537c3490c9b
korilla2 <kingdoss@naver.com>
2022-01-02 12:36:45 +0900

    커밋

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-N = int(input())
-N_list = [-num for num in range(1, N+1) if num % 2 == 0]
-print(N_list)
+# 저는 이거부터 어렵네요..
+t = int(input())
+
+for test in range(1, t+1):
+    n = map(int, input().split())
+    sum = 0
+    for i in n:
+        if i % 2 != 0:
+            sum += i
+    print("#", str(test), str(sum))
