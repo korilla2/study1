ded8da6fee6e3f80f35f7508d58f725bbc5eb3fb
korilla2 <kingdoss@naver.com>
2022-01-10 00:45:08 +0900

    커밋

--- 2주차문제/5.미로.py
+++ 2주차문제/5.미로.py
@@ -1,47 +1,52 @@
 from collections import deque
 
-end = int(input())
-
-di = [-1, 1, 0, 0]
-dj = [0, 0, -1, 1]
-
-
-def find_end(arr, i, j):
-
-    stack = deque()
-    start = arr[i][j]
-    stack.append((i, j))
-    while stack:
-        i, j = stack.pop()
-
-        for i in range(4):
-            ni = i + di[i]
-            nj = j + dj[i]
-
-            if ni < 0 or ni >= len(arr) or nj < 0 or nj >= len(arr):
-                continue
-            if arr[ni][nj] == 1:
-                continue
-            if arr[ni][nj] == 0:
-                arr[ni][nj] = 1
-                stack.append((ni, nj))
-            if arr[ni][nj] == 2:
-                return 1
-    return 0
+dx_list = [-1, 1, 0, 0]
+dy_list = [0, 0, -1, 1]
 
 
 def find_start(arr):
     for i in range(len(arr)):
         for j in range(len(arr)):
-            if arr[i][j] == 3:
-                find_end(arr, i, j)
+            if arr[i][j] == 2:
+                result = find_end(arr, i, j)
+    if result == 3:
+        return 1
+    else:
+        return 0
 
 
-for j in range(end):
-    T = int(input())
+def find_end(arr, i, j):
+    queue = deque()
+    queue.append((i, j))
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            dx = x + dx_list[i]
+            dy = y + dy_list[i]
+
+            if dx < 0 or dy < 0 or dx > len(arr) - 1 or dy > len(arr) - 1:
+                continue
+            elif arr[dx][dy] == 1:
+                continue
+            elif arr[dx][dy] == 3:
+                while len(queue) > 0:
+                    queue.popleft()
+                break
+            elif arr[dx][dy] == 0:
+                arr[dx][dy] = 1
+                queue.append((dx, dy))
+    if dx < 0 or dy < 0 or dx > len(arr) - 1 or dy > len(arr) - 1:
+        return False
+    return arr[dx][dy]
+
+
+T = int(input())
+
+
+for i in range(T):
+    N = int(input())
     arr = []
-    for i in range(T):
+    for j in range(N):
         arr.append(list(map(int, input())))
-    print(arr)
-    print(find_start(arr))
-https: // swexpertacademy.com/main/learn/course/lectureProblemViewer.do
+    print(f'#{i+1} {find_start(arr)}')
